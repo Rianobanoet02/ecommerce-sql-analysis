c98889d4326a4e4e1f77f60d5f7e2828679c7039
Rianobanoet02 <rianobanoetb@gmail.com>
2026-04-03 16:32:20 +0800

    update analysis

--- analyis.sql
+++ analyis.sql
@@ -54,3 +54,54 @@ from (
 -- 100% customer hanya melakukan 1 transaksi
 -- Tidak terdapat repeat customer
 -- Namun hasil ini perlu dikaji lebih lanjut apakah disebabkan oleh karakter bisnis atau struktur dataset
+
+
+-- ==============================
+-- 4. REVENUE TREND (MONTHLY)
+-- ==============================
+select date_format(o.order_purchase_timestamp,'%y-%m') as order_month,
+		sum(p.payment_value) as total_revenue
+        from orders o
+        join order_payments p
+        on o.order_id =p.order_id
+        group by order_month
+        order by order_month;
+        
+-- Insight:
+-- Revenue pada awal periode sangat rendah (sekitar 19–252)
+-- Kemudian meningkat signifikan dan stabil di kisaran ~100 ribu per bulan
+-- Hal ini menunjukkan adanya fase awal dengan aktivitas rendah,
+-- diikuti oleh periode pertumbuhan dan stabilisasi
+
+-- 5. ORDER TREND (MONTHLY)
+-- ==============================
+SELECT 
+    DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS order_month,
+    COUNT(order_id) AS total_orders
+FROM orders
+GROUP BY order_month
+ORDER BY order_month;
+
+-- Insight:
+-- Mayoritas jumlah order per bulan berada pada kisaran sekitar 2000 transaksi
+-- Terdapat beberapa bulan dengan jumlah order yang sangat rendah (<10),
+-- kemungkinan merupakan fase awal atau akhir periode data
+-- Pola revenue menunjukkan hubungan yang sejalan dengan jumlah order
+-- Hal ini mengindikasikan bahwa revenue lebih dipengaruhi oleh volume transaksi
+-- dibandingkan nilai transaksi per order
+
+
+-- ==============================
+-- 6. AVERAGE ORDER VALUE
+-- ==============================
+select AVG(orders_value) from (
+	select order_id,
+	sum(payment_value) as orders_value 
+		from order_payments
+		group by order_id)t
+	;
+-- Insight:
+-- Average order value berada pada kisaran 160
+-- Dibandingkan dengan total revenue dan jumlah order,
+-- terlihat bahwa revenue lebih dipengaruhi oleh volume transaksi
+-- daripada nilai per transaksi
